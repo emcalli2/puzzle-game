bf850f0728baa63ba0ef47dac67d4a6ac0dc5158
emcalli2 <142628454+emcalli2@users.noreply.github.com>
2025-05-01 13:25:36 -0700

    Update puzzle.py port

--- puzzle.py
+++ puzzle.py
@@ -39,4 +39,4 @@ def puzzle():
     return render_template_string(HTML, scrambled=SCRAMBLED, result=result)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
